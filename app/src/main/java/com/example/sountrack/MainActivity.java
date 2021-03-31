5471f3e034f5750c313757c161442aa4f0f6b423
Myca <kathlynmyca.delacruz@gmail.com>
2021-03-31 03:56:30 -0600

    commit

--- app/src/main/java/com/example/sountrack/MainActivity.java
+++ app/src/main/java/com/example/sountrack/MainActivity.java
@@ -19,13 +19,15 @@ import static android.Manifest.permission.RECORD_AUDIO;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnRecord1, btnRecord2, btnRecord3, btnStop, btnPlay;
-    private MediaPlayer mplayer1, mplayer2, mplayer3;
+    private Button btnRecord1, btnRecord2, btnRecord3, btnStop, btnPlay1, btnPlay2, btnPlay3, guitar, dramatic,orchestra;
+    private MediaPlayer mplayer1, mplayer2, mplayer3, mp1,mp2,mp3;
     private MediaRecorder mRecorder1, mRecorder2, mRecorder3;
     private static final String LOG_TAG = "AudioRecording";
     private static String mFilename1 = null;
     private static String mFilename2 = null;
     private static String mFilename3 = null;
+    private int recorder = 0;
+
 
     public static final int REQUEST_AUDIO_PERMISSION_CODE = 1;
 
@@ -34,30 +36,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      btnRecord1 = this.findViewById(R.id.record1);
-      btnRecord2 = this.findViewById(R.id.record2);
-      btnRecord3 = this.findViewById(R.id.record3);
-      btnStop = this.findViewById(R.id.stop);
-      btnPlay = this.findViewById(R.id.play);
 
-      mFilename1 = Environment.getExternalStorageDirectory().getAbsolutePath();
-      mFilename1 += "/record1.3gp";
+        guitar = this.findViewById(R.id.music1);
+        dramatic = this.findViewById(R.id.music2);
+        orchestra = this.findViewById(R.id.music3);
 
-      mFilename2 = Environment.getExternalStorageDirectory().getAbsolutePath();
-      mFilename2 += "/record2.3gp";
+        btnRecord1 = this.findViewById(R.id.record1);
+        btnRecord2 = this.findViewById(R.id.record2);
+        btnRecord3 = this.findViewById(R.id.record3);
+        btnStop = this.findViewById(R.id.stop);
+        btnPlay1 = this.findViewById(R.id.play);
+        btnPlay2 = this.findViewById(R.id.play2);
+        btnPlay3 = this.findViewById(R.id.play3);
 
-      mFilename2 = Environment.getExternalStorageDirectory().getAbsolutePath();
-      mFilename2 += "/record3.3gp";
+        mFilename1 = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFilename1 += "/record1.3gp";
+
+        mFilename2 = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFilename2 += "/record2.3gp";
+
+        mFilename3 = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFilename3 += "/record3.3gp";
 
 
-      // for recording sounds
+// for recording sounds
         btnRecord1.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 if(CheckPermissions()) {
                     btnRecord1.setEnabled(false);
-                    btnPlay.setEnabled(false);
+                    btnPlay1.setEnabled(false);
+                    btnPlay2.setEnabled(false);
+                    btnPlay3.setEnabled(false);
                     btnStop.setEnabled(true);
 
                     mRecorder1 = new MediaRecorder();
@@ -71,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
                         Log.e(LOG_TAG,"prepare() failed" );
                     }
                     mRecorder1.start();
+                    recorder=1;
                 } else {
                     RequestPermissions();
 
@@ -83,6 +95,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(CheckPermissions()) {
+                    btnRecord2.setEnabled(false);
+                    btnPlay1.setEnabled(false);
+                    btnPlay2.setEnabled(false);
+                    btnPlay3.setEnabled(false);
+                    btnStop.setEnabled(true);
                     mRecorder2 = new MediaRecorder();
                     mRecorder2.setAudioSource(MediaRecorder.AudioSource.MIC);
                     mRecorder2.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
@@ -94,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
                         Log.e(LOG_TAG,"prepare() failed" );
                     }
                     mRecorder2.start();
+                    recorder=2;
                 } else {
                     RequestPermissions();
 
@@ -106,6 +124,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(CheckPermissions()) {
+                    btnRecord3.setEnabled(false);
+                    btnPlay1.setEnabled(false);
+                    btnPlay2.setEnabled(false);
+                    btnPlay3.setEnabled(false);
+                    btnStop.setEnabled(true);
                     mRecorder3 = new MediaRecorder();
                     mRecorder3.setAudioSource(MediaRecorder.AudioSource.MIC);
                     mRecorder3.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
@@ -117,6 +140,7 @@ public class MainActivity extends AppCompatActivity {
                         Log.e(LOG_TAG,"prepare() failed" );
                     }
                     mRecorder3.start();
+                    recorder=3;
                 } else {
                     RequestPermissions();
 
@@ -127,36 +151,47 @@ public class MainActivity extends AppCompatActivity {
         btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnRecord1.setEnabled(true);
                 btnStop.setEnabled(false);
-                btnPlay.setEnabled(true);
+                btnPlay1.setEnabled(true);
+                btnPlay2.setEnabled(true);
+                btnPlay3.setEnabled(true);
+                switch (recorder){
+                    case 1:
+                        btnRecord1.setEnabled(true);
+                        mRecorder1.stop();
+                        mRecorder1.release();
+                        mRecorder1 = null;
+                        break;
+                    case 2:
+                        btnRecord2.setEnabled(true);
+                        mRecorder2.stop();
+                        mRecorder2.release();
+                        mRecorder2 = null;
 
-                mRecorder1.stop();
-                mRecorder1.release();
-                mRecorder1 = null;
-
-                mRecorder2.stop();
-                mRecorder2.release();
-                mRecorder2 = null;
-
-            //    mRecorder3.stop();
-            //    mRecorder3.release();
-            //    mRecorder3 = null;
-            }
+                        break;
+                    case 3:
+                        btnRecord3.setEnabled(true);
+                        mRecorder3.stop();
+                        mRecorder3.release();
+                        mRecorder3 = null;
+                        break;
+                    default:
+                        break;
+                }}
         });
 
-        btnPlay.setOnClickListener(new View.OnClickListener() {
+        btnPlay1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 btnRecord1.setEnabled(true);
                 btnStop.setEnabled(false);
-                btnPlay.setEnabled(true);
+                btnPlay1.setEnabled(true);
 
                 if (mplayer1 != null) {
                     mplayer1.release();
                     mplayer1 = null;
                 }
-                 mplayer1 = new MediaPlayer();
+                mplayer1 = new MediaPlayer();
                 try {
                     mplayer1.setDataSource(mFilename1);
                     mplayer1.prepare();
@@ -167,51 +202,108 @@ public class MainActivity extends AppCompatActivity {
                     mplayer1 = null;
                 }
 
-//                if (mplayer2 != null) {
-//                    mplayer2.release();
-//                    mplayer2 = null;
-//
-//                }
-//                mplayer2 = new MediaPlayer();
-//                try {
-//                    mplayer2.setDataSource(mFilename2);
-//                    mplayer2.prepare();
-//                    mplayer2.start();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                    mplayer2.release();
-//                    mplayer2 = null;
-//                }
-//
-//                if (mplayer3 == null) {
-//                    mplayer3.release();
-//                    mplayer3 = null;
-//
-//                }
-//                mplayer3 = new MediaPlayer();
-//                try {
-//                    mplayer3.setDataSource(mFilename3);
-//                    mplayer3.prepare();
-//                    mplayer3.start();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                    mplayer3.release();
-//                    mplayer3 = null;
-//                }
             }
         });
+        btnPlay2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mplayer2 != null) {
+                    mplayer2.release();
+                    mplayer2 = null;
+
+                }
+                mplayer2 = new MediaPlayer();
+                try {
+                    mplayer2.setDataSource(mFilename2);
+                    mplayer2.prepare();
+                    mplayer2.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    mplayer2.release();
+                    mplayer2 = null;
+                }
+            }
+        });
+        btnPlay3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mplayer3 != null) {
+                    mplayer3.release();
+                    mplayer3 = null;
+
+                }
+                mplayer3 = new MediaPlayer();
+                try {
+                    mplayer3.setDataSource(mFilename3);
+                    mplayer3.prepare();
+                    mplayer3.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    mplayer3.release();
+                    mplayer3 = null;
+                }}
+        });
     }
-            public boolean CheckPermissions() {
-                int resultExternalStorage = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
-                int resultRecordAudio = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
-                return (resultExternalStorage == PackageManager.PERMISSION_GRANTED &&
-                        resultRecordAudio == PackageManager.PERMISSION_GRANTED);
+
+    public boolean CheckPermissions() {
+        int resultExternalStorage = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int resultRecordAudio = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
+        return (resultExternalStorage == PackageManager.PERMISSION_GRANTED &&
+                resultRecordAudio == PackageManager.PERMISSION_GRANTED);
+    }
+    private void RequestPermissions() {
+        ActivityCompat.requestPermissions(MainActivity.this, new String[] {RECORD_AUDIO, WRITE_EXTERNAL_STORAGE},
+                REQUEST_AUDIO_PERMISSION_CODE);
+    }
+
+     public void toggleGuitar(View v) {
+
+         mp1 = MediaPlayer.create(this, R.raw.guitar);
+         mp1.setLooping(false);
+         mp1.seekTo(0);
+         mp1.setVolume(0.5f, 0.5f);
+
+         try {
+            if (mp1.isPlaying()) {
+                mp1.stop();
+                mp1.release();
+                mp1 = MediaPlayer.create(this,R.raw.guitar);
+            }else {
+                mp1.start();
             }
-            private void RequestPermissions() {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[] {RECORD_AUDIO, WRITE_EXTERNAL_STORAGE},
-                        REQUEST_AUDIO_PERMISSION_CODE);
+        } catch(Exception e) {e.printStackTrace();}
+     }
+
+    public void toggleDramatic(View v) {
+        mp2 = MediaPlayer.create(this, R.raw.dramatic);
+        mp2.setLooping(false);
+        mp2.seekTo(0);
+        mp2.setVolume(0.5f, 0.5f);
+        try {
+            if (mp2.isPlaying()) {
+                mp2.stop();
+                mp2.release();
+                mp2 = MediaPlayer.create(this,R.raw.dramatic);
+            }else {
+                mp2.start();
             }
+        } catch(Exception e) {e.printStackTrace();}
+    }
 
-
-
+    public void toggleOrchestra(View v) {
+        mp3 = MediaPlayer.create(this, R.raw.orchestra);
+        mp3.setLooping(false);
+        mp3.seekTo(0);
+        mp3.setVolume(0.5f, 0.5f);
+        try {
+            if (mp3.isPlaying()) {
+                mp3.stop();
+                mp3.release();
+                mp3 = MediaPlayer.create(this,R.raw.orchestra);
+            }else {
+                mp3.start();
+            }
+        } catch(Exception e) {e.printStackTrace();}
+    }
 }
